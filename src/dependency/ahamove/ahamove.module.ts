98bba3c36ac09bd6ddba45f5dbfdd87102c92cbc
NHT <hoangtuan910@gmail.com>
2023-12-18 20:33:34 +0700

    put token to .env & change the Ahamove API to v3

--- src/dependency/ahamove/ahamove.module.ts
+++ src/dependency/ahamove/ahamove.module.ts
@@ -1,9 +1,10 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AhamoveService } from './ahamove.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, ConfigModule],
   providers: [AhamoveService],
   exports: [AhamoveService],
 })
