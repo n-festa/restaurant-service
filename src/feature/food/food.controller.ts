3a7088397fc554941223bdc8bf760e6f912c5a68
NHT <hoangtuan910@gmail.com>
2023-12-19 18:02:44 +0700

    get basic data without sku for menu item

--- src/feature/food/food.controller.ts
+++ src/feature/food/food.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
 import { FoodService } from './food.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { FlagsmithService } from 'src/dependency/flagsmith/flagsmith.service';
 
 @Controller()
 export class FoodController {
-  constructor(private readonly foodService: FoodService) {}
+  constructor(
+    @Inject('FLAGSMITH_SERVICE') private readonly flagService: FlagsmithService,
+    private readonly foodService: FoodService,
+  ) {}
+
+  @MessagePattern({ cmd: 'get_food_detail_by_id' })
+  async getFoodDetailById(id: number) {
+    if (this.flagService.isFeatureEnabled('fes-15-get-food-detail')) {
+      return await this.foodService.getFoodDetailById(id);
+    } else {
+    }
+  }
 }
