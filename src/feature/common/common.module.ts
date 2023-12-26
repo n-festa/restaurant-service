9609108fbadd604a2fccd8761cd095234fa2b54d
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2023-12-26 17:18:19 +0700

    allow common service access db (#19)
    * allow common service access db
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/feature/common/common.module.ts
+++ src/feature/common/common.module.ts
@@ -2,10 +2,16 @@ import { Global, Module } from '@nestjs/common';
 import { CommonService } from './common.service';
 import { FoodModule } from '../food/food.module';
 import { RestaurantModule } from '../restaurant/restaurant.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestaurantExt } from 'src/entity/restaurant-ext.entity';
 
 @Global()
 @Module({
-  imports: [FoodModule, RestaurantModule],
+  imports: [
+    TypeOrmModule.forFeature([RestaurantExt]),
+    FoodModule,
+    RestaurantModule,
+  ],
   exports: [CommonService],
   providers: [CommonService],
   controllers: [],
