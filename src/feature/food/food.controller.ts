c1a8385b144bdec00194860b031600a4134c2837
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-02-10 20:53:44 +0700

    FES-42-get-hot-foods (#52)
    * create fake data, dto and reading fake data logic
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/feature/food/food.controller.ts
+++ src/feature/food/food.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, HttpException, Inject } from '@nestjs/common';
 import { FoodService } from './food.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { FlagsmithService } from 'src/dependency/flagsmith/flagsmith.service';
 import { GetSideDishRequest } from './dto/get-side-dish-request.dto';
 import { GetFoodDetailResponse } from './dto/get-food-detail-response.dto';
+import { GetHotFoodResponse } from './dto/get-hot-food-response.dto';
+import { FoodDTO } from 'src/dto/food.dto';
 
 @Controller()
 export class FoodController {
@@ -26,4 +28,26 @@ export class FoodController {
   async getSideDishByMenuItemId(data: GetSideDishRequest) {
     return await this.foodService.getSideDishByMenuItemId(data);
   } //end of getSideDishByMenuItemId
+
+  @MessagePattern({ cmd: 'get_hot_food' })
+  async getHotFood(): Promise<GetHotFoodResponse> {
+    const res = new GetHotFoodResponse(200, '');
+    try {
+      const foods: FoodDTO[] = await this.foodService.getHotFoodFromEndPoint();
+      res.statusCode = 200;
+      res.message = 'Get hot food successfully';
+      res.data = foods;
+    } catch (error) {
+      if (error instanceof HttpException) {
+        res.statusCode = error.getStatus();
+        res.message = error.getResponse();
+        res.data = null;
+      } else {
+        res.statusCode = 500;
+        res.message = error.toString();
+        res.data = null;
+      }
+    }
+    return res;
+  } // end of getHotFood
 }
