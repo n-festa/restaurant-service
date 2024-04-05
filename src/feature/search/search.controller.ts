3d79bbfc92e3748a67c72d44f4150e55dc0cc660
nfesta2023 <142601504+nfesta2023@users.noreply.github.com>
2024-04-05 09:02:07 +0700

    Fes 63 improve the search api with filter (#103)
    * add search logic
    
    * add command to allow create function in mysql
    
    ---------
    
    Co-authored-by: NHT <hoangtuan910@gmail.com>

--- src/feature/search/search.controller.ts
+++ src/feature/search/search.controller.ts
@@ -1,9 +1,20 @@
-import { Controller, Inject } from '@nestjs/common';
+import { Controller, Inject, UseFilters } from '@nestjs/common';
 import { SearchService } from './search.service';
 import { MessagePattern } from '@nestjs/microservices';
 import { FlagsmithService } from 'src/dependency/flagsmith/flagsmith.service';
 import { SearchFoodByNameRequest } from './dto/search-food-by-name-request.dto';
 import { SearchResult } from 'src/dto/search-result.dto';
+import {
+  ResultType,
+  SearchFoodRequest,
+  SortType,
+} from './dto/search-food-request.dto';
+import {
+  SearchFoodResponse,
+  FoodDTO as SearchFoodDTO,
+  SrestaurantDTO as SearchRestaurantDTO,
+} from './dto/search-food-response.dto';
+import { CustomRpcExceptionFilter } from 'src/filters/custom-rpc-exception.filter';
 
 @Controller()
 export class SearchController {
@@ -27,5 +38,73 @@ export class SearchController {
     );
 
     //CURRENT LOGIC
+  }
+
+  @MessagePattern({ cmd: 'search_food' })
+  @UseFilters(new CustomRpcExceptionFilter())
+  async searchFood(data: SearchFoodRequest): Promise<SearchFoodResponse> {
+    const {
+      keyword,
+      ISO_language_code,
+      lat,
+      long,
+      result_type,
+      sort_type,
+      filter = [],
+      offset,
+      page_size,
+      distance_limit_m = 10000,
+      base_distance_for_grouping_m = 200,
+    } = data;
+
+    const fullResult: SearchFoodDTO[] | SearchRestaurantDTO[] =
+      await this.searchService.searchFoodInGeneral(
+        keyword.trim(),
+        ISO_language_code,
+        Number(lat),
+        Number(long),
+        distance_limit_m,
+        base_distance_for_grouping_m,
+        result_type,
+        filter,
+      );
+
+    //Sort the result based on the sort type
+    if (result_type == ResultType.FOOD) {
+      switch (sort_type) {
+        case SortType.RELEVANCE:
+          //Do nothing
+          break;
+        case SortType.PRICE_ASC:
+          fullResult.sort((a, b) => a.price - b.price);
+          break;
+        case SortType.PRICE_DESC:
+          fullResult.sort((a, b) => b.price - a.price);
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    //Filter with the offset & page_size
+    const finalResult = fullResult.slice(offset, page_size);
+
+    const result: SearchFoodResponse = {
+      results: finalResult,
+      keyword: keyword,
+      ISO_language_code: ISO_language_code,
+      lat: lat,
+      long: long,
+      result_type: result_type,
+      sort_type: sort_type,
+      filter: filter,
+      offset: offset + finalResult.length,
+      total: fullResult.length,
+      distance_limit_m: distance_limit_m,
+      base_distance_for_grouping_m: base_distance_for_grouping_m,
+    };
+
+    return result;
   }
 }
